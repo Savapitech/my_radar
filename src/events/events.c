eb308abca92421112e98da1b76c019113d9108d1
savalet <savinien.petitjean@gmail.com>
2025-01-05 17:52:07 +0100

    add: hitboxes only showed when L is pressed

--- src/events/events.c
+++ src/events/events.c
@@ -14,6 +14,8 @@ int event_handler(rf_t *rf)
     sfEvent event = { 0 };
 
     while (sfRenderWindow_pollEvent(rf->window, &event)) {
+        if (event.type == sfEvtKeyPressed && event.key.code == sfKeyL)
+            rf->show_hitboxes = !rf->show_hitboxes;
         if (event.type == sfEvtClosed)
             sfRenderWindow_close(rf->window);
     }
--- src/render/display.c
+++ src/render/display.c
@@ -9,15 +9,28 @@
 
 #include "radar.h"
 
+static
+void display_planes(rf_t *rf, size_t i)
+{
+    sfRenderWindow_drawSprite(rf->window, rf->planes[i].sprite, NULL);
+    if (rf->show_hitboxes)
+        sfRenderWindow_drawRectangleShape(rf->window,
+            rf->planes[i].hitbox, NULL);
+}
+
 int display_all(rf_t *rf)
 {
     sfRenderWindow_clear(rf->window, sfBlack);
-    for (int i = 0; i < 10; i++)
-        if (rf->planes[i].active) {
-            sfRenderWindow_drawSprite(rf->window, rf->planes[i].sprite, NULL);
-            sfRenderWindow_drawRectangleShape(rf->window,
-                rf->planes[i].hitbox, NULL);
-        }
+    sfRenderWindow_drawSprite(rf->window, rf->background.sprite, NULL);
+    for (size_t i = 0; i < rf->planes_nb; i++)
+        if (rf->planes[i].active)
+            display_planes(rf, i);
+    for (size_t i = 0; i < rf->towers_nb; i++) {
+        sfRenderWindow_drawSprite(rf->window, rf->towers[i].sprite, NULL);
+        if (rf->show_hitboxes)
+            sfRenderWindow_drawCircleShape(rf->window,
+                rf->towers[i].t_hitbox, NULL);
+    }
     sfRenderWindow_display(rf->window);
     return RETURN_SUCCESS;
 }
